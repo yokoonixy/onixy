10a8680cedd5815cb4322f943f1f7a0c5ecaeb48
yokoonixy <secondlast.blog@gmail.com>
2017-04-29 17:18:35 +0700

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="lima-situs-shorten-url-dengan-bayaran.html"}
-if(x=="2"){xxx="cara-menggabungkan-adsense-ke-safelink.html"}
-if(x=="3"){xxx="cara-membuat-shortener-url-dengan.html"}
-if(x=="4"){xxx="how-to-make-blog-safelink-converter.html"}
-if(x=="5"){xxx="how-to-get-money-from-blogger.html"}a_to_ve[i].href="https://idconvertin.blogspot.com/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2017/04/lima-situs-shorten-url-dengan-bayaran.html"}
+if(x=="2"){xxx="/2017/04/cara-menggabungkan-adsense-ke-safelink.html"}
+if(x=="3"){xxx="/2017/04/cara-membuat-shortener-url-dengan.html"}
+if(x=="4"){xxx="/2017/04/how-to-make-blog-safelink-converter.html"}
+if(x=="5"){xxx="/2017/04/how-to-get-money-from-blogger.html"}a_to_ve[i].href="https://idconvertin.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
